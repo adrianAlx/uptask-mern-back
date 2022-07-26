1f4feca6124761547086ebb9116dca8b808dfdbb
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-07-26 08:18:32 -0500

    [ADD]: User model added

---
+++ src/models/User.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import { Schema, model } from 'mongoose';
+import bcryptjs from 'bcryptjs';
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required!'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required!'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required!'],
+      trim: true,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    confirmed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+UserSchema.pre('save', async function (next) {
+  // If the pass is already hashed, it don't re-hashet it
+  if (!this.isModified('password')) return next();
+
+  const hash = await bcryptjs.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+UserSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  user.uid = user._id;
+
+  delete user.password;
+  delete user._id;
+  delete user.createdAt;
+  delete user.updatedAt;
+
+  return user;
+};
+
+export default model('User', UserSchema);
--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+import User from './User';
+
+export { User };
